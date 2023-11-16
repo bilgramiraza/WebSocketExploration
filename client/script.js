4f567bf0b923c1f2405215df86e4626b0a2b5a6c
bilgramiraza <bilgramiraza@yahoo.com>
2023-11-16 14:58:02 +0530

    Setup client

---
+++ client/script.js
@@ -0,0 +1,30 @@
+const url = 'ws://localhost:69420/Websocket';
+const mywsServer = new WebSocket(url);
+
+const myMessages = document.getElementById('messages');
+const myInput = document.getElementById('message');
+const sendBtn = document.getElementById('send');
+
+sendBtn.disabled  = true;
+sendBtn.addEventListener('click', sendMsg, false);
+
+function sendMsg(){
+  const text = myInput.value;
+  msgGeneration(text, 'Client');
+  mywsServer.send(text);
+}
+
+function msgGeneration(msg, from){
+  const newMessage = document.createElement('h5');
+  newMessage.innerText = `${from}: ${msg}`;
+  myMessages.appendChild(newMessage);
+}
+
+mywsServer.onopen = function(){
+  sendBtn.disabled = false;
+}
+
+mywsServer.onmessage = function(event){
+  const { data } = event;
+  msgGeneration(data, 'Server');
+}
